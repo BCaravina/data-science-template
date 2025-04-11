4237f14b6cb2bd6944bc7f4572582586c8e0de2b
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-04-11 10:17:58 -0300

    Began exploring the data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -14,14 +14,39 @@ single_file_gyr = pd.read_csv(
 )
 
 # --------------------------------------------------------------
-# List all data in data/raw/MetaMotion
+# List all data in data/raw/MetaMotion; glob is a library that lists all the files (can also specify certain extension - *.csv below) within a directory
 # --------------------------------------------------------------
 
+files = glob("../../data/raw/MetaMotion/*.csv")
+len(files)
 
 # --------------------------------------------------------------
-# Extract features from filename
+# Extract features from filename and adding them to the dataframe
 # --------------------------------------------------------------
 
+# experimenting with a variable before applying it to the whole list
+data_path = "../../data/raw/MetaMotion/"
+f = files[0]
+# spliting the whole string to get the desired information we want
+participant = f.split("-")[0].replace(data_path, "")
+exercise_label = f.split("-")[1]
+# removing any characters from the right that are 123 (since participants did 3 sets)
+intensity_category = f.split(
+    "-"
+)[
+    2
+].rsplit(
+    "123"
+)[
+    0
+]  # for some reason this variable was being returned as a 1 item list, so I added the [0] at the end to fix it
+
+
+df = pd.read_csv(f)
+# creating columns and adding them to the df
+df["participant"] = participant
+df["exercise_label"] = exercise_label
+df["intensity_category"] = intensity_category
 
 # --------------------------------------------------------------
 # Read all files
