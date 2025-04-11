49e7bbe9bcb8f143622eed1045fa81b03af79ed3
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-04-11 15:35:43 -0300

    combined all steps into a function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -99,13 +99,53 @@ del gyr_df["epoch (ms)"]
 del gyr_df["time (01:00)"]
 del gyr_df["elapsed (s)"]
 
-acc_df
-gyr_df
-
 # --------------------------------------------------------------
-# Turn into function
+# Turn everything into a function
 # --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
 
+
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    data_path = "../../data/raw/MetaMotion/"
+    for f in files:
+        participant = f.split("-")[0].replace(data_path, "")
+        exercise_label = f.split("-")[1]
+        intensity_match = re.search(r"(heavy|medium|standing|sitting)", f)
+        intensity_category = intensity_match.group(1) if intensity_match else "unknown"
+
+        df = pd.read_csv(f)
+        df["participant"] = participant
+        df["exercise_label"] = exercise_label
+        df["intensity_category"] = intensity_category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+        if "Gyroscope" in f:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+
+    return acc_df, gyr_df
+
+
+acc_df, gyr_df = read_data_from_files(files)
 
 # --------------------------------------------------------------
 # Merging datasets
