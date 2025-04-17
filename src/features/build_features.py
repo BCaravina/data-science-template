8049a866e9320398cfddd1321e486b8682b73bee
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-04-17 11:00:01 -0300

    Applied Fourier Transformation and did data clustering

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
+from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 # --------------------------------------------------------------
 # Load data
@@ -141,26 +142,133 @@ df_squared["gyr_r"] = np.sqrt(gyr_r)
 
 subset = df_squared[df_squared["set"] == 14]
 subset[["acc_r", "gyr_r"]].plot(subplots=True)
+
 # --------------------------------------------------------------
-# Temporal abstraction
+# Temporal abstraction -> using rolling averages w/ pandas
 # --------------------------------------------------------------
+
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
+
+# adding the temporal features to the acc and gyr columns as well as the r squared columns (gyr_r and acc_r)
+predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+
+# window size determines how many values we want to use to predict the mean/standard deviation
+ws = int(
+    1000 / 200
+)  # step size is of 200ms, so to get a ws of 1 second we need 5 step sizes
+
+# looping through all the columns and compute the mean and stand dev
+for col in predictor_columns:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+# since the ws uses the previous 4 values to compute the mean and std, this brings up two problems: first 4 rows won't have values computed, AND bench press data will be used to compute the means for squats or deadlifts or rows...so we need to fix that by making SUBSETS for each label
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+
+    for col in predictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+
+# turning the list with the subsets into a single dataframe again (overriding the OG dataframe)
+df_temporal = pd.concat(df_temporal_list)
+
+df_temporal.info()  # we can see we lost some of the data on the new columns, but that is good because we are avoiding the "spillage" of mismatched label data into another label type (ex: bench into squat OR squat into row, etc)
+
+# using a random subset to visualize the data
+subset[["acc_r", "acc_y (g)_temp_mean_ws_5", "acc_y (g)_temp_std_ws_5"]].plot()
+subset[["gyr_r", "gyr_y (deg/s)_temp_mean_ws_5", "gyr_y (deg/s)_temp_std_ws_5"]].plot()
+
+# --------------------------------------------------------------
+# Frequency features -> applying the Discrete Fourier Transformation (DFT)
+# --------------------------------------------------------------
+
+df_frequency = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+# frequency sampling rate and window size
+fs = int(1000 / 200)
+ws = int(2800 / 200)
+
+df_frequency = FreqAbs.abstract_frequency(df_frequency, ["acc_y (g)"], ws, fs)
+
+# repeating the steps of the step above, creating empty list and populating it with the subset lists then concatenating them all back into the dataframe
+df_frequency_list = []
+for s in df_frequency["set"].unique():
+    print(f"Applying Fourier transformations to set {s}")
+    subset = df_frequency[df_frequency["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_frequency_list.append(subset)
+
+df_frequency = pd.concat(df_frequency_list).set_index("epoch (ms)", drop=True)
+
+# --------------------------------------------------------------
+# Dealing with overlapping windows -> avoiding possible overfitting
+# --------------------------------------------------------------
+
+dr_frequency = df_frequency.dropna()
+df_frequency = df_frequency.iloc[
+    ::2
+]  # removing EVERY OTHER row of the dataframe -> reducing data in 50%
 
 
 # --------------------------------------------------------------
-# Frequency features
+# Clustering -> K-Means clustering
 # --------------------------------------------------------------
 
+df_cluster = df_frequency.copy()
+cluster_columns = ["acc_x (g)", "acc_y (g)", "acc_z (g)"]
+k_values = range(2, 10)
+inertias = []
 
-# --------------------------------------------------------------
-# Dealing with overlapping windows
-# --------------------------------------------------------------
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
 
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.xlabel("k")
+plt.ylabel("Sum of squared distances")
+plt.show()
 
-# --------------------------------------------------------------
-# Clustering
-# --------------------------------------------------------------
+# selecting the optimal n_cluster number after visualizing the plot above
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
 
+# visually checking if the selected clusters make sense
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x (g)"], subset["acc_y (g)"], subset["acc_z (g)"], label=c)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+# visually comparing the splitting of the data by clusters vs EXERCISE LABEL clusters
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for l in df_cluster["exercise_label"].unique():
+    subset = df_cluster[df_cluster["exercise_label"] == l]
+    ax.scatter(subset["acc_x (g)"], subset["acc_y (g)"], subset["acc_z (g)"], label=l)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+# comparing the 3D plots above we can see which cluster number corresponds to which exercises
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
